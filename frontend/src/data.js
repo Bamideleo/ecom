305ce549da4d73d7a14fb66933ed746b87abfa7f
Bamideleo <oluwabukolaoluwaseun@gmail.com>
2022-11-08 06:41:28 +0100

    Add code to Ecom

---
+++ frontend/src/data.js
@@ -0,0 +1,83 @@
+ const  data = {
+    products:[
+        {
+            id:'1',
+            name:'Nike Slime Shirt',
+            category:'Shirt',
+            image:'/images/ant3.PNG',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'Brand New Product'
+        },
+
+        {
+            id:'2',
+            name:'Puma Slime Pant',
+            category:'Pant',
+            image:'/images/ant3.PNG',
+            price:50,
+            brand:'Pume',
+            rating:4.2,
+            numReviews:40,
+            description:'Brand New Product'
+        },
+
+        {
+            id:'3',
+            name:'Nike Slim Pant',
+            category:'Pant',
+            image:'/images/ant3.PNG',
+            price:90,
+            brand:'Nike',
+            rating:4.0,
+            numReviews:29,
+            description:'Brand New Product'
+        },
+
+        {
+            id:'4',
+            name:'Addias Slime Shirt',
+            category:'Shirt',
+            image:'/images/ant3.PNG',
+            price:150,
+            brand:'Addias',
+            rating:4.8,
+            numReviews:20,
+            description:'Brand New Product'
+        },
+
+
+
+
+        {
+            id:'5',
+            name:'Lacoste Free Pant',
+            category:'Pant',
+            image:'/images/ant3.PNG',
+            price:70,
+            brand:'Lacoste',
+            rating:5,
+            numReviews:30,
+            description:'Brand New Product'
+        },
+
+
+
+
+
+        {
+            id:'6',
+            name:'Addias Slime Shirt',
+            category:'Shirt',
+            image:'/images/ant3.PNG',
+            price:120,
+            brand:'Addias',
+            rating:4.5,
+            numReviews:10,
+            description:'Brand New Product'
+        },
+    ]
+}
+export default data
